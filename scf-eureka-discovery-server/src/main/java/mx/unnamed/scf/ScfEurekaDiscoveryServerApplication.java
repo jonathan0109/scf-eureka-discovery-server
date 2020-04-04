9c85c61c2e67232c939994098c4f791432b342c5
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-03 18:32:55 -0600

    enable server and configure properties

--- scf-eureka-discovery-server/src/main/java/mx/unnamed/scf/ScfEurekaDiscoveryServerApplication.java
+++ scf-eureka-discovery-server/src/main/java/mx/unnamed/scf/ScfEurekaDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package mx.unnamed.scf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ScfEurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
