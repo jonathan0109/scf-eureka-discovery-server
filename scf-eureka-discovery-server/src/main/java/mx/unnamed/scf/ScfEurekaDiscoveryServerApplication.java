5747fefff98957dd6cda38b51972ac5788f5d9d5
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-03 18:11:22 -0600

    skeleton

---
+++ scf-eureka-discovery-server/src/main/java/mx/unnamed/scf/ScfEurekaDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package mx.unnamed.scf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScfEurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScfEurekaDiscoveryServerApplication.class, args);
+	}
+
+}
